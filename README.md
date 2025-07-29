22a63c8236949e89567be69659090aba2ad0ea3f
Nadawoo <nadatruc@gmail.com>
2025-07-30 00:04:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# What is EtwinBar?
+Etwinbar is a part of the Eternaltwin project (https://eternaltwin.org/)
+Etwinbar allows you to insert on your own site a footer common to all of the Eternaltwin games.
+
+# How to install the Eternaltwin footer on my site?
+## 1) Install the EtwinBar library
+Put the **/etwinbar** folder in the appropriate place of your site. It can be the root or a subfolder.
+
+## 2) Set up the HTML elements
+Insert this line where you want inside the <footer> of your page :
+```html
+<section id="etwinFooter"></section>
+```
+This is where the list of the games will be added.
+
+Insert this line inside the <head> of your page :
+```html
+<link rel="stylesheet" href="etwinbar/css/etwinBar.css">
+```
+Adapt the path if you have placed the **/etwinbar** folder elsewhere than at the root of your site.
+
+Insert this line before the end of your <body> :
+```html
+<script type="text/javascript" src="etwinbar/js/etwinBar.js" async></script>
+```
+Adapt the path if you have placed the **/etwinbar** folder elsewhere than at the root of your site.
+
+# Example
+Here is a minimalistic structure with the 3 elements to insert :
+```html
+<html>
+	<head>
+		<link rel="stylesheet" href="etwinbar/css/etwinBar.css">
+	</head>
+	<body>
+		
+		<!-- The main content of your site is here -->
+		
+		<footer>
+			<section id="etwinFooter"></section>
+		</footer>
+		
+		<script type="text/javascript" src="etwinbar/js/etwinBar.js" async></script>
+		
+	</body>
+</html>
+```
+
+You can find a complete example of implementation in the **demo.htm** page.
+
+# How to change the style of the footer?
+The CSS provided with EtwinBar is minimalistic, as we assume you want to apply styles in accordance with the design of your site. If you want to add or change the styles, put your rules in your own CSS files. Don't put them in the CSS files of EtwinBar, as they would be erased in case of update.
---
+++ etwinbar/js/etwinBar.js
@@ -0,0 +1,65 @@
+
+addEtwinFooter();
+
+
+/**
+ * Adds the links to the Eternaltwin games in the footer of the page
+ */
+function addEtwinFooter() {
+	
+	const sites = [
+		{
+		name: "MyHordes",
+		url: "https://myhordes.eu/jx/public/welcome",
+		description: "Le premier jeu de survie zombie gratuit se déroulant dans un monde hostile peuplé de morts-vivants !"
+		},
+		{
+		name: "eMush",
+		url: "https://emush.eternaltwin.org/",
+		description: "Vous êtes sur le point de vous réveiller à bord du Daedalus avec 15 autres rebelles. Comme vous, ils fuient le Mush, un champignon parasite qui ravage la Terre et menace l'Humanité."
+		},
+		{
+		name: "DinoRPG",
+		url: "https://dinorpg.eternaltwin.org/",
+		description: "Élevez vos dinoz dans un jeu d'aventure extraordinaire !"
+		},
+		{
+		name: "Neoparc",
+		url: "https://neoparc.eternaltwin.org/",
+		description: "Combattez d'autres Dinoz et gagnez ainsi petit à petit de l'expérience lors de combats sans merci !"
+		},
+		{
+		name: "Eternalfest",
+		url: "https://eternalfest.net/",
+		description: "Plus de 200 niveaux ! Une grande variété d'ennemis très très méchants !"
+		},
+		{
+		name: "Eternal Kingdom",
+		url: "https://kingdom.eternaltwin.org/",
+		description: "Faites prospérer votre capitale. Recrutez des soldats et devenez... Empereur !"
+		},
+		{
+		name: "Directquiz",
+		url: "https://www.directquiz.org/",
+		description: "Mettez votre culture à rude épreuve dans Direct Quiz, le jeu de quiz le plus déjanté du web !"
+		},
+		{
+		name: "ePopotamo",
+		url: "https://epopotamo.eternaltwin.org/",
+		description: "Le jeu de mots relax jouable en toute situation !"
+		}
+	];
+
+	const etwinFooter = document.querySelector('#etwinFooter');
+	const sitesContainer = document.createElement('ul');
+	
+	etwinFooter.innerHTML = `<h2>Autres jeux de Eternaltwin</h2>`;
+	etwinFooter.appendChild(sitesContainer);
+	
+	// Write each site in a new <li>
+	sites.forEach(site => {
+		const newSiteItem = document.createElement('li');		
+		newSiteItem.innerHTML = `<a href="${site.url}" title="${site.description}" target="_blank">${site.name}</a>`;		
+		sitesContainer.appendChild(newSiteItem);
+	});
+}
