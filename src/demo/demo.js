096984826f69e21c59f5ce1b3891b4371cbd9553
Nadawoo <nadatruc@gmail.com>
2025-08-07 13:24:34 +0200

    Styles for the demo moved to a separate CSS file

--- src/demo/demo.js
+++
@@ -1,3 +0,0 @@
-import EtwinBar from "/src/index.js"
-const footer = new EtwinBar();
-footer.init();
---
+++ src/demo/script.js
@@ -0,0 +1,7 @@
+/*
+ * This is a simulation of how *your* script initiates the EtwinBar library.
+ * You can freely modify this code, it's not a part of the library.
+ */
+import EtwinBar from "/src/index.js"
+const footer = new EtwinBar();
+footer.init();
